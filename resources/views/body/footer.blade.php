1ee7f851ac0fcfff92f4028937745f94462cf7e9
AymaneHassini <aymane.hassini@gmail.com>
2024-09-07 15:19:57 +0100

    added the admin template, segmented the dashboard page and customized the header and footer

---
+++ resources/views/body/footer.blade.php
@@ -0,0 +1,23 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6">
+                <span style="font-weight: bold;">
+                <strong style="color: black;">
+    <script>document.write(new Date().getFullYear())</script> &copy; Yazaki Track-
+</strong><a href="" style="color: #E60012;">Yazaki Kenitra</a>
+                </span>
+            </div>
+            <div class="col-md-6">
+                <div class="text-md-end footer-links d-none d-sm-block">
+                    <a href="https://www.yazaki-group.com/en/company/index.html#:~:text=We%20are%20the%20Yazaki%20Group,conditioners%20and%20solar%20thermal%20appliances." target="_blank">About Us</a>
+                    <a href="https://www.yazaki-group.com/en/gas/support/index.html" target="_blank">Help</a>
+                    <a href="https://www.yazaki-europe.com/fr/about-us/yazaki-worldwide" target="_blank">Contact Us</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
+
